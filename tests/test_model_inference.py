67a28df197faa18da21c0574e87a75dbe2484358
Abramov Denis <Denis.Abramov@skoltech.ru>
2025-10-21 19:52:24 +0300

    add test model inference

---
+++ tests/test_model_inference.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import numpy as np
+from src.model.classifier import CatDogClassifier
+
+def test_model_inference_smoke():
+    clf = CatDogClassifier()
+    img = Image.new("RGB", (224, 224), color=(120, 120, 120))
+    out = clf.predict(img)
+    assert set(out.keys()) == {"label", "prob"}
+    assert out["label"] in {"cat", "dog"}
+    assert 0.0 <= out["prob"] <= 1.0
+
+def test_numpy_input():
+    clf = CatDogClassifier()
+    arr = np.ones((224, 224, 3), dtype=np.uint8) * 150
+    out = clf.predict(arr)
+    assert isinstance(out["label"], str)
